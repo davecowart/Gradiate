a308cf2b928a842eaa80eaa4cf27b2afd75d4c67
Dave Cowart <davecowart@gmail.com>
2012-09-19 18:22:45 -0400

    removed support for IE

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ Simple jQuery plugin to generate a gradient based on an element's existing backg
 ```javascript
 $('.color').gradiate();
 ```
+or
+```javascript
+//set the multiplier used to generate the opposing color
+//(defaults to 0.8)
+$('.color').gradiate(0.75);
+```
--- jquery.gradiate.js
+++ jquery.gradiate.js
@@ -1,14 +1,13 @@
-(function($) {
-
+(function ($) {
     function parseRGB(rgb, multiplier) {
         var values = rgb.match(/\d+/g);
         multiplier = multiplier || 0.8;
-        for(var i in values) {
-            values[i] = Math.round(values[i] * multiplier);    
+        for (var i in values) {
+            values[i] = Math.round(values[i] * multiplier);
         }
         return 'rgb(' + values.join(', ') + ')';
     }
-    
+
     function setGradient(element, startColor, endColor) {
         var $this = $(element);
         $this.css('background', startColor);
@@ -16,18 +15,18 @@
         $this.css('background', '-webkit-gradient(linear, left top, left bottom, color-stop(0%,' + startColor + '), color-stop(99%,' + endColor + '));');
         $this.css('background', '-webkit-linear-gradient(top, ' + startColor + ' 0%,' + endColor + ' 99%)');
         $this.css('background', '-o-linear-gradient(top, ' + startColor + ' 0%,' + endColor + ' 99%)');
-        $this.css('background', '-ms-linear-gradient(top,' + startColor + ' 0%,' + endColor + ' 99%)');
         $this.css('background', 'linear-gradient(to bottom, ' + startColor + ' 0%,' + endColor + ' 99%)');
     }
-    
-    $.fn.gradiate = function() {
-        return this.each(function() {
-              var $this = $(this);
-              console.log($this.attr('id'));            
-              var color = $this.css('background-color');
-              var otherColor = parseRGB(color, 0.75);
-              setGradient($this, color, otherColor);
+
+    $.fn.gradiate = function (multiplier) {
+        if ($.browser.msie) {
+            return this;
+        }
+        return this.each(function () {
+            var $this = $(this);
+            var color = $this.css('background-color');
+            var otherColor = parseRGB(color, multiplier);
+            setGradient($this, color, otherColor);
         });
     };
-    
 })(jQuery);
